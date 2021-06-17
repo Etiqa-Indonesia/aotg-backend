012a6f2b59dc6ced60ec9d34f63a74528f9decf7
Rhega Rofiat <rhega.rofiat@etiqa.co.id>
2021-06-17 16:26:40 +0700

    Index Change by Rhega

--- app/models/index.js
+++ app/models/index.js
@@ -34,6 +34,7 @@ db.User = require("./user.model.js")(sequelize, Sequelize);
 db.Agent = require("./agent.model.js")(sequelize, Sequelize);
 db.UserLog = require("./user.log.model")(sequelize, Sequelize);
 db.Quotation = require("./quotation.model")(sequelize, Sequelize);
+db.Customer = require("./customer.model")(sequelize, Sequelize);
 db.QuotationLog = require("./quotation.log.model")(sequelize, Sequelize);
 db.QuotationMV = require("./quotation.mv.model")(sequelize, Sequelize);
 db.Coverage = require("./coverage.model")(sequelize, Sequelize);
@@ -42,5 +43,10 @@ db.AgentCat = require("./agentcat.model")(sequelize, Sequelize);
 db.AgentTopro = require("./agenttopro.model")(sequelize, Sequelize);
 db.RateTab = require("./ratetab.model")(sequelize, Sequelize);
 db.RCAllowed = require("./rcallowed.model")(sequelize, Sequelize);
+db.AgentHandler = require("./agenthandler.model")(sequelize, Sequelize);
+db.PlateCode = require("./platecode.model")(sequelize, Sequelize);
+
+db.VehicleType = require("./vehicletype.model")(sequelize, Sequelize);
+db.VTAllowed = require("./vtallowed.model")(sequelize, Sequelize);
 
 module.exports = db;
